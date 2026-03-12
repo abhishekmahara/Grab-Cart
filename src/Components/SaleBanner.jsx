363bb3cadc7f7f2f0035e1a06c099fcf8ff2f49d
Abhishek <abhishekmahara9877@gmail.com>
2026-03-12 20:18:08 +0530

    fix: fixed some text errors

--- src/Components/SaleBanner.jsx
+++ src/Components/SaleBanner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SaleBanner = () => {
   return (
     <div className="w-full bg-black text-white text-center text-xs md:text-base py-2 tracking-wide">
-     Welcome to Grabcart. Enjoy Free Delivery in 2–4 Days & Easy Returns
+     Welcome to Grabcart. Enjoy Free Delivery in 3–5 Days & Easy Returns
     </div>
   );
 };
